7232bf3cc98a9deac38fb646c5778ca6e5cff850
Aaron Ulrich <ace@gis.ca>
2017-11-16 13:50:33 -0800

    postgres updates

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const express = require('express');
 var bodyParser = require('body-parser');  // this is needed to parse JSON
 const app = express();
 
+//const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/metais';
+//const connectionString = 'postgres://localhost:5432/metais';
+//const connectionString = "postgres://postgres:T00thp1ck7!@localhost:5432/metais";
+//const connectionString = process.env.DATABASE_URL;
 var pg = require('pg');
+
+const config = {
+    user: 'postgres',
+    database: 'metais',
+    password: 'T00thp1ck7!',
+    port: 5432
+};
+const pool = new pg.Pool(config);
+
 
 /*
 // If not HTTPS, redirect that request to the same url but with HTTPS
@@ -28,17 +41,27 @@ app.use(bodyParser.json());
 app.listen(process.env.PORT || 8080);
 
 
+
 // Test the database
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+app.get('/db', function (req, res) {
+
+  // var pool = new pg.Pool()
+  pool.connect(function (err, client, done) {
+    if (err) {
+      console.error('could not connect to postgres', err);
+    }
+
+    client.query('SELECT * FROM test_table', function (err, result) {
+        done();
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
     });
   });
+  // pool.end()
+
 });
 
 
